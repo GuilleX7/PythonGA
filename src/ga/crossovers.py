7f1d3a97d9ed77e825c040892b980718ce09aafc
CarmenMariaMP <62175263+CarmenMariaMP@users.noreply.github.com>
2021-06-15 22:16:40 +0200

    Mutations and crossover
    simple_mutator ,single_point_crossover and triple_point_crossover made

--- src/ga/crossovers.py
+++ src/ga/crossovers.py
@@ -11,7 +11,10 @@ def single_point_crossover(chromosome_a, chromosome_b):
         chromosome_a (list<number>): First chromosome
         chromosome_b (list<number>): Second chromosome
     """
-    pass
+    crossover_point=random.randrange(1,self.individuals_length-1)
+    new_chromosome_1= chromosome_a[:crossover_point] + chromosome_b[crossover_point:] 
+    new_chromosome_2= chromosome_b[:crossover_point] + chromosome_a[crossover_point:] 
+    return [new_chromosome_1,new_chromosome_2]
 
 def triple_point_crossover(chromosome_a, chromosome_b):
     """
@@ -26,7 +29,48 @@ def triple_point_crossover(chromosome_a, chromosome_b):
         chromosome_a (list<number>): First chromosome
         chromosome_b (list<number>): Second chromosome
     """
-    pass
+    crossover_point_1=random.randrange(1,self.individuals_length-1)
+    crossover_point_2=random.randrange(1,self.individuals_length-1)
+    crossover_point_3=random.randrange(1,self.individuals_length-1)
+    if(crossover_point_1==crossover_point_2 && crossover_point_2==crossover_point_3):
+        new_chromosome_1= chromosome_a[:crossover_point_1] + chromosome_b[crossover_point_1:] 
+        new_chromosome_2= chromosome_b[:crossover_point_1] + chromosome_a[crossover_point_1:] 
+    elif(crossover_point_1<crossover_point_2 && crossover_point_1<crossover_point_3):
+        if(crossover_point_2<crossover_point_3):
+            new_chromosome_1= chromosome_a[:crossover_point_1] + chromosome_b[crossover_point_1:crossover_point_2]
+         + chromosome_a[crossover_point_2:crossover_point_3]  + chromosome_b[crossover_point_3:]  
+        new_chromosome_2= chromosome_b[:crossover_point_1] + chromosome_a[crossover_point_1:crossover_point_2]
+         + chromosome_b[crossover_point_2:crossover_point_3]  + chromosome_a[crossover_point_3:]
+         else:
+             new_chromosome_1= chromosome_a[:crossover_point_1] + chromosome_b[crossover_point_1:crossover_point_3]
+         + chromosome_a[crossover_point_3:crossover_point_2]  + chromosome_b[crossover_point_2:]  
+        new_chromosome_2= chromosome_b[:crossover_point_1] + chromosome_a[crossover_point_1:crossover_point_3]
+         + chromosome_b[crossover_point_3:crossover_point_2]  + chromosome_a[crossover_point_2:]
+    elif(crossover_point_2<crossover_point_1 && crossover_point_2<crossover_point_3):
+        if(crossover_point_1<crossover_point_3):
+            new_chromosome_1= chromosome_a[:crossover_point_2] + chromosome_b[crossover_point_2:crossover_point_1]
+         + chromosome_a[crossover_point_1:crossover_point_3]  + chromosome_b[crossover_point_3:]  
+        new_chromosome_2= chromosome_b[:crossover_point_2] + chromosome_a[crossover_point_2:crossover_point_1]
+         + chromosome_b[crossover_point_1:crossover_point_3]  + chromosome_a[crossover_point_3:]
+         else:
+             new_chromosome_1= chromosome_a[:crossover_point_2] + chromosome_b[crossover_point_2:crossover_point_3]
+         + chromosome_a[crossover_point_3:crossover_point_1]  + chromosome_b[crossover_point_1:]  
+        new_chromosome_2= chromosome_b[:crossover_point_2] + chromosome_a[crossover_point_2:crossover_point_3]
+         + chromosome_b[crossover_point_3:crossover_point_1]  + chromosome_a[crossover_point_1:] 
+    elif(crossover_point_3<crossover_point_1 && crossover_point_3<crossover_point_2):
+        if(crossover_point_1<crossover_point_2):
+            new_chromosome_1= chromosome_a[:crossover_point_3] + chromosome_b[crossover_point_3:crossover_point_1]
+         + chromosome_a[crossover_point_1:crossover_point_2]  + chromosome_b[crossover_point_2:]  
+        new_chromosome_2= chromosome_b[:crossover_point_3] + chromosome_a[crossover_point_3:crossover_point_1]
+         + chromosome_b[crossover_point_1:crossover_point_2]  + chromosome_a[crossover_point_2:]
+         else:
+             new_chromosome_1= chromosome_a[:crossover_point_3] + chromosome_b[crossover_point_3:crossover_point_2]
+         + chromosome_a[crossover_point_2:crossover_point_1]  + chromosome_b[crossover_point_1:]  
+        new_chromosome_2= chromosome_b[:crossover_point_3] + chromosome_a[crossover_point_3:crossover_point_2]
+         + chromosome_b[crossover_point_2:crossover_point_1]  + chromosome_a[crossover_point_1:]   
+
+    
+    return [new_chromosome_1,new_chromosome_2]
 
 def uniform_crossover(chromosome_a, chromosome_b):
     """
--- src/ga/mutators.py
+++ src/ga/mutators.py
@@ -1,8 +1,13 @@
-def simple_mutator(chromosome):
+def simple_mutator(self,chromosome,probability):
     """
     Changes some genes with a given probability.
 
     Args:
         chromosome (list<number>): chromosome to be mutated
+        probability (number): mutation probability
     """
-    pass
+    mutated_chromosome=list(chromosome) 
+        for i in range(len(chromosome)):
+            if random.random() < probability :
+                mutated_chromosome[i] = random.choice(self.genes)
+        return mutated_chromosome
