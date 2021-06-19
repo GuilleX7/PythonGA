739708030746ecbc3970b796df12bbaf411c61a7
GuilleX7 <guillermox7@gmail.com>
2021-06-19 21:16:19 +0200

    Added main functionality

--- src/ga/main.py
+++ src/ga/main.py
@@ -1,7 +1,98 @@
+from math import sin
 from multiknapsack.multiknapsack_problem import MultiknapsackProblem
+from crossovers.crossovers import single_point_crossover, triple_point_crossover, uniform_crossover
 import io
+import random
 
-mk_problem = MultiknapsackProblem.of_random_objects(50, 1, 100, 5)
-mk_problem.run()
-with io.open("dog_breeds.txt", mode="w", encoding="utf-8") as f:
-    f.write(mk_problem.get_detailed_solution())
+def calculate_best_balanced_distribution(objects, n_min, n_max):
+    """
+    Calculates the best distribution of the given objects in a given
+    amount of knapsacks in the range [n_min, n_max]. It returns a list with
+    both the best individual found and the n for which the problem has been
+    calculated.
+
+    Args:
+        objects (list<number>): A list with the objects to be distributed
+        n_min (number): The minimum amount of knapsacks for the objects to be distributed into
+        n_max (number): The maximum amount of knapsakcs for the objects to be distributed into
+
+    Returns:
+        tuple<list<number>, number>: A tuple containing the best individual found in first place
+            and the number of knapsacks of the problem in second place
+    """
+    with io.open("./reports/best_balanced_distribution_summary.txt", "w") as summary_file:
+        mk_problems = [MultiknapsackProblem(objects, n) for n in range(n_min, n_max + 1)]
+        mk_solutions = []
+        for i, mk_problem in enumerate(mk_problems):
+            mk_problem.problem_instance.properties.selection_amount = 500
+            mk_problem.problem_instance.properties.mutation_probability = 0.05
+            mk_problem.run()
+            winner = mk_problem.problem_instance.get_best_individual()
+            solution = mk_problem.get_solution()
+            with io.open("./reports/best_balanced_distribution_n_" + str(i + n_min) + ".txt", "w", encoding="utf-8") as individual_report_file:
+                individual_report_file.write(mk_problem.get_pretty_solution())
+            
+            summary_file.write("Found solution for n=" + str(i + n_min) + ": knapsacks " + str(solution[0]) + " with distance to perfection " + str(solution[1]) + "\n")
+            print("Found solution for n=" + str(i + n_min) + ": knapsacks " + str(solution[0]) + " with distance to perfection " + str(solution[1]))
+            
+            mk_solutions.append([i, winner, solution[1]])
+        
+        best_solution = min(mk_solutions, key=lambda x: x[2])
+        
+        summary_file.write("=============================\nBEST solution is n=" + str(best_solution[0] + n_min))
+        print("=============================\nBEST solution is n=" + str(best_solution[0] + n_min))
+        
+        return [best_solution[1], best_solution[0]]
+
+def experiment_distributions(objects, knapsack_amount, conditions):
+    """
+    Allows to run instances of the same problem under different conditions.
+
+    Args:
+        objects (): [description]
+        conditions (list<dict<string, ?>>): A list containing dictionaries with the following problem properties:
+            selection_amount, mutation_probability, halting_generations, crossover
+    """
+    with io.open("./reports/experiment_summary.txt", "w") as summary_file:
+        mk_problems = [MultiknapsackProblem(objects, knapsack_amount) for _ in range(len(conditions))]
+        for i, mk_problem in enumerate(mk_problems):
+            mk_problem.problem_instance.properties.selection_amount = conditions[i]["selection_amount"]
+            mk_problem.problem_instance.properties.mutation_probability = conditions[i]["mutation_probability"]
+            mk_problem.problem_instance.properties.halting_generations = conditions[i]["halting_generations"]
+            mk_problem.problem_instance.crossover = conditions[i]["crossover"]
+            mk_problem.run()
+            solution = mk_problem.get_solution()
+            with io.open("./reports/experiment_distribution_" + str(i) + ".txt", "w", encoding="utf-8") as individual_report_file:
+                individual_report_file.write("Selection amount: " + str(conditions[i]["selection_amount"]) + "\n")
+                individual_report_file.write("Mutation probability [0, 1]: " + str(conditions[i]["mutation_probability"]) + "\n")
+                individual_report_file.write("Halting amount of generations: " + str(conditions[i]["halting_generations"]) + "\n")
+                individual_report_file.write("Crossover function: " + str(conditions[i]["crossover"]) + "\n\n")
+                individual_report_file.write(mk_problem.get_pretty_solution())
+            print("\nEXPERIMENT " + str(i))
+            print("Selection amount: " + str(conditions[i]["selection_amount"]))
+            print("Mutation probability [0, 1]: " + str(conditions[i]["mutation_probability"]))
+            print("Halting amount of generations: " + str(conditions[i]["halting_generations"]))
+            print("Crossover function: " + str(conditions[i]["crossover"]))
+            
+            summary_file.write("Experiment " + str(i) + ": knapsacks " + str(solution[0]) + " with distance to perfection " + str(solution[1]) + "\n")
+            print("Result: knapsacks " + str(solution[0]) + " with distance to perfection " + str(solution[1]))
+
+# MAIN PROGRAM
+# 1. First, we try to distribute a list of random objects in a given number of knapsacks, n, in a range between 2 and 10
+#print("1. DISTRIBUTION OF RANDOM OBJECTS IN A RANGE 'n' OF KNAPSACKS")
+#num_objects = random.randint(100, 200)
+#print("Selected amount of objects: " + str(num_objects))
+#calculate_best_balanced_distribution([random.randint(1, 100) for _ in range(num_objects)], 2, 10)
+
+# 2. We perform some experiments using different properties for each instance of the problem
+print("\n2. EXPERIMENTS")
+num_objects = random.randint(1000, 1500)
+print("Selected amount of objects: " + str(num_objects))
+experiment_distributions(
+    [random.randint(1, 100) for _ in range(num_objects)],
+    4,
+    [
+        {"selection_amount": 100, "mutation_probability": 0.05, "halting_generations": 100, "crossover": single_point_crossover},
+        {"selection_amount": 300, "mutation_probability": 0.05, "halting_generations": 300, "crossover": triple_point_crossover},
+    ]
+)
--- src/ga/multiknapsack/multiknapsack_problem.py
+++ src/ga/multiknapsack/multiknapsack_problem.py
@@ -6,6 +6,14 @@ from problem.problem_instance import ProblemInstance
 
 class MultiknapsackProblem:
     def __init__(self, objects, knapsack_amount):
+        """
+        Creates a Multiknapsack Problem instance with a given list of objects and
+        a given knapsack amount.
+
+        Args:
+            objects (list<number>): List of objects (weights)
+            knapsack_amount (number): Amount of knapsacks
+        """
         self.objects = objects
         self.knapsack_number = knapsack_amount
         self.perfect_average_weight = sum(objects) / knapsack_amount
@@ -13,19 +21,51 @@ class MultiknapsackProblem:
         self.problem = Problem(list(range(knapsack_amount)), len(objects), self.decode, self.fitness)
         self.problem_instance = ProblemInstance(self.problem)
         self.problem_instance.properties.optimization_type = "MIN"
-        self.problem_instance.properties.mutation_probability = 0.1
 
     @staticmethod
     def of_random_objects(object_amount, min_weight, max_weight, knapsack_amount):
+        """
+        Creates a new Multiknapsack Problem with a random list of objects using
+        the given parameters.
+
+        Args:
+            object_amount (number): Amount of objects
+            min_weight (number): Minimum weight of an object
+            max_weight (number): Maximum weight of an object
+            knapsack_amount (number): Amount of knapsacks
+
+        Returns:
+            MultiknapsackProblem: The created instance
+        """
         return MultiknapsackProblem([random.randint(min_weight, max_weight) for _ in range(object_amount)], knapsack_amount)
 
     def decode(self, chromosome):
+        """
+        Decodes the given solution to the problem
+
+        Args:
+            chromosome (list<number>): The individual to be decoded
+
+        Returns:
+            list<list<number>>: A list of lists, each representing a knapsack, with the indices of
+                the objects that are inside them 
+        """
         knaps = [[] for _ in range(self.knapsack_number)]
         for i, selected_knap in enumerate(chromosome):
             knaps[selected_knap].append(i)
         return knaps
 
     def fitness(self, chromosome):
+        """
+        Calculates the fitness of a given individual as the squared distance to
+        the arithmetic mean.
+
+        Args:
+            chromosome (list<number>): The individual to be fitnessed
+
+        Returns:
+            number: The fitness of the individual
+        """
         knaps_spaces = [0] * self.knapsack_number
         for i, selected_knap in enumerate(chromosome):
             knaps_spaces[selected_knap] += self.objects[i]
@@ -33,9 +73,37 @@ class MultiknapsackProblem:
         return distance_from_perfect_weight
 
     def run(self):
+        """
+        Runs the problem until it halts.
+        """
         self.problem_instance.run_until_halt()
 
-    def get_detailed_solution(self):
+    def get_solution(self):
+        """
+        Gets the best solution found after the problem is halted.
+
+        Returns:
+            tuple<list<number>, number>: A list containing in first place a list with the spaces
+                occupied in each knapsack, and the fitness of the individual for which the solution
+                was calculated from.
+        """
+        winner = self.problem_instance.get_best_individual()
+        final_knaps = self.problem_instance.decode_individual(winner)
+        final_spaces = []
+        for i, knap in enumerate(final_knaps):
+            total_knap_weight = 0
+            for i in knap:
+                total_knap_weight += self.objects[i]
+            final_spaces.append(total_knap_weight)
+        return [final_spaces, self.problem_instance.fitness_individual(winner)]
+
+    def get_pretty_solution(self):
+        """
+        Prints the solution in a pretty and human-readable way.
+
+        Returns:
+            string: The pretty-printed solution
+        """
         solution = ""
         winner = self.problem_instance.get_best_individual()
         final_knaps = self.problem_instance.decode_individual(winner)
