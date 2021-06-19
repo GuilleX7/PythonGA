12571ecc0eb05e0a34fb360ad19a515c9fccbdcf
GuilleX7 <guillermox7@gmail.com>
2021-06-19 03:06:44 +0200

    Improved genetic algorithm representation and updated doc

--- src/ga/main.py
+++ src/ga/main.py
@@ -1,68 +1,56 @@
 import random
 from collections import namedtuple
+from typing import final
 from crossovers.crossovers import triple_point_crossover, uniform_crossover
 from problem.problem import Problem
 from problem.problem_instance import ProblemInstance
 from selectors.selectors import roulette_wheel_selector, tournament_selector
 
-num_objects = 20
+num_objects = 50
 object_min_weight = 1
 object_max_weight = 100
-num_knaps = 5
-knap_min_space = 1
-knap_max_space = 150
+num_knaps = 2
 
-KnapObject = namedtuple("KnapObject", "weight")
-
-objects = [KnapObject(random.randint(object_min_weight, object_max_weight)) for _ in range(num_objects)]
-knap_available_spaces = [random.randint(knap_min_space, knap_max_space) for _ in range(num_knaps)]
-
-objects_total_weight=0
-for i, obj in enumerate(objects):
-    objects_total_weight += objects[i].weight
-average_weights=objects_total_weight/num_knaps
+objects = [random.randint(object_min_weight, object_max_weight) for _ in range(num_objects)]
+perfect_average_weight = sum(objects) / num_knaps
 
 
 def knap_decode(chromosome):
-    knaps_spaces = [0] * num_knaps
-    difference = 0
-    total_weight = 0
+    knaps = [[] for _ in range(num_knaps)]
     for i, selected_knap in enumerate(chromosome):
-        if selected_knap == -1:
-            continue
-        knaps_spaces[selected_knap] += objects[i].weight
-        total_weight += objects[i].weight
-
-    for i in range(0, num_knaps ):
-        difference += (abs(knaps_spaces[i] - average_weights))**2
-
-    return [knaps_spaces, difference, (objects_total_weight-total_weight)]
+        knaps[selected_knap].append(i)
+    return knaps
 
 
 def knap_fitness(chromosome):
     knaps_spaces = [0] * num_knaps
-    difference = 0
-    total_weight=0
     for i, selected_knap in enumerate(chromosome):
-        if selected_knap == -1:
-            continue
-        knaps_spaces[selected_knap] += objects[i].weight
-        total_weight += objects[i].weight
-
-    for i in range(0, num_knaps):
-        difference += (abs(knaps_spaces[i] - average_weights))**2
-
-    return difference *100 + (objects_total_weight-total_weight) *1000
+        knaps_spaces[selected_knap] += objects[i]
+    distance_from_perfect_weight = sum([abs(knaps_spaces[i] - perfect_average_weight) ** 2 for i in range(num_knaps)])
+    return distance_from_perfect_weight
 
 
-ga_problem = Problem(list(range(-1, num_knaps)), num_objects, knap_decode, knap_fitness)
+ga_problem = Problem(list(range(num_knaps)), num_objects, knap_decode, knap_fitness)
 ga_problem_inst = ProblemInstance(ga_problem)
 ga_problem_inst.properties.optimization_type = "MIN"
-ga_problem_inst.properties.mutation_probability = 0.01
+ga_problem_inst.properties.mutation_probability = 0.1
 
 while not ga_problem_inst.is_halted():
     ga_problem_inst.calculate_next_generation()
-    print("Best individual of generation " + str(ga_problem_inst.generation_count) + ": " + str(ga_problem_inst.decode_individual(ga_problem_inst.get_best_individual())))
+    best = ga_problem_inst.get_best_individual()
+    print("Best individual of generation " + str(ga_problem_inst.generation_count) + ": " + str(ga_problem_inst.decode_individual(best)) + " with fitness " + str(ga_problem_inst.fitness_individual(best)))
 
+print("=====================================================")
 winner = ga_problem_inst.get_best_individual()
-print("Best solution found: " + str(ga_problem_inst.decode_individual(winner)))
+final_knaps = ga_problem_inst.decode_individual(winner)
+final_spaces = []
+for i, knap in enumerate(final_knaps):
+    print("KNAP Nº" + str(i + 1))
+    total_knap_weight = 0
+    for i in knap:
+        print("Object nº" + str(i + 1) + " with weight " + str(objects[i]))
+        total_knap_weight += objects[i]
+    final_spaces.append(total_knap_weight)
+    print("Total weight: " + str(total_knap_weight) + "\n")
+
+print("Perfect average weight: " + str(perfect_average_weight) + ", obtained: " + str(final_spaces) + " with distance (fitness): " + str(ga_problem_inst.fitness_individual(winner)))
--- src/ga/problem/problem_instance.py
+++ src/ga/problem/problem_instance.py
@@ -43,15 +43,42 @@ class ProblemInstance:
         self.generation_count = 0
 
     def create_population(self):
+        """
+        Generates a population for the problem instance using random genes. The population
+        will fit the properties.selection_amount number.
+
+        Returns:
+            list<list<number>>: Initial population for the problem instance
+        """
         return [self.create_individual() for _ in range(self.properties.selection_amount)]
 
     def create_individual(self):
+        """
+        Generates a single individual (chromosome) for the problem instance using random
+        genes.
+
+        Returns:
+            list<number>: A single individual for the problem instance
+        """
         return random.choices(self.problem.genes, k=self.problem.individuals_length)
 
     def is_halted(self):
+        """
+        Returns whether the problem has been already halted or not, this is, if
+        the actual generation has reached the maximum generation count or not.
+
+        Returns:
+            boolean: Whether the actual generation has reached the maximum number
+                of generations
+        """
         return self.properties.halting_generations <= self.generation_count
 
     def calculate_next_generation(self):
+        """
+        Calculates the next generation of individuals. It doesn't take into account whether
+        the problem instance has already halted or not, so it can calculate subsequent generations
+        after finishing.
+        """
         self.parents = self.selector(self.parents, self.problem.fitness, self.properties)
         self.childs = [self.crossover(c1, c2) for [c1, c2] in [random.choices(self.parents, k=2) for _ in range(self.properties.selection_amount)]]
         self.childs = [self.mutator(chromosome, self.problem.genes, self.properties) for chromosome in self.childs]
@@ -59,14 +86,45 @@ class ProblemInstance:
         self.generation_count += 1
 
     def run_until_halt(self):
+        """
+        Runs the problem instance and calculates generation until the halting condition is met.
+        """
         while not self.is_halted():
             self.calculate_next_generation()
 
     def get_best_individual(self):
+        """
+        Gets the best individual from the current generation.
+
+        Returns:
+            list<number>: Best individual in the current generation
+        """
         cmp = max if self.properties.optimization_type == "MAX" else min
         return cmp(self.childs, key=lambda x: self.problem.fitness(x))
 
     def decode_individual(self, individual):
+        """
+        Auxiliary method. Decodes an individual.
+
+        Args:
+            individual (list<number>): A given individual for this problem
+
+        Returns:
+            ?: The solution after being decoded for this problem
+        """
         return self.problem.decode(individual)
+
+    def fitness_individual(self, individual):
+        """
+        Auxiliary method. Calculates the fitness of an individual.
+
+        Args:
+            individual (list<number>): A given individual for this problem
+
+        Returns:
+            number: The fitness of the given individual with respect to
+                this problem
+        """
+        return self.problem.fitness(individual)
 
 
